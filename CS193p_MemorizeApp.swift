6b8130a42a33e5bcbce2f63833a3d79a6cf2a0ae
HipDiscovery <Troytyork@yahoo.com>
2021-07-11 17:07:47 -0500

    Initial commit

---
+++ CS193p_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_MemorizeApp.swift
+//  CS193p-Memorize
+//
+//  Created by Troy York on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
