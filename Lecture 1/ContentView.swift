f78cceb53cc40df69344c8d9c32c61ee65cae53e
HipDiscovery <Troytyork@yahoo.com>
2021-07-11 20:25:00 -0500

    Lecture Folders

---
+++ Lecture 1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CS193p-Memorize
+//
+//  Created by Troy York on 7/11/21.
+//
+
+//import SwiftUI
+//
+//struct L1ContentView: View {
+//var body: some View {
+//    ZStack {            RoundedRectangle(cornerRadius: 20)
+//        .stroke(lineWidth: 3)
+//        Text("Hello World!")
+//}
+//            .padding(.horizontal)
+//            .foregroundColor(.red)
+//}
+//}
+//
+//struct L1ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        L1ContentView()
+//    }
+//}
