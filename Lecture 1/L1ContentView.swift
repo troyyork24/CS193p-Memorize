036bc04f6775c638b1e864b44a076e312f2ff76b
Troy York <66917393+troyyork24@users.noreply.github.com>
2021-08-01 02:43:00 -0500

    Update L1ContentView.swift

--- Lecture 1/L1ContentView.swift
+++ Lecture 1/L1ContentView.swift
@@ -5,21 +5,22 @@
 //  Created by Troy York on 7/11/21.
 //
 
-//import SwiftUI
-//
-//struct L1ContentView: View {
-//var body: some View {
-//    ZStack {            RoundedRectangle(cornerRadius: 20)
-//        .stroke(lineWidth: 3)
-//        Text("Hello World!")
-//}
-//            .padding(.horizontal)
-//            .foregroundColor(.red)
-//}
-//}
-//
-//struct L1ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        L1ContentView()
-//    }
-//}
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    ZStack {            
+       RoundedRectangle(cornerRadius: 20)
+         .stroke(lineWidth: 3)
+      Text("Hello World!")
+    }
+           .padding(.horizontal)
+           .foregroundColor(.red)
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+   static var previews: some View {
+       ContentView()
+   }
+}
